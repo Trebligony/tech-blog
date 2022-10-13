749a893e8683dd28ee132a97ce63276d3829cbe2
Ala Owerri <irete@alaowerri.com>
2022-10-12 20:46:36 -0400

    jawsdb connection update in config file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,13 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
